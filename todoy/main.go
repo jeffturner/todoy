088a0aee4c7ef1087ff600610e689bc0217aebc6
Jeff Turner <github@jeffturner.net>
2015-11-23 19:46:10 -0500

    refactored server back into main file

--- todoy/main.go
+++ todoy/main.go
@@ -2,39 +2,54 @@ package main
 
 import (
 	"fmt"
+	"net/http"
+	"strings"
 	"time"
 
 	"gopkg.in/redis.v3"
 )
 
 func main() {
+	hostAndPort := ":8080"
+
 	redisClient := redis.NewClient(&redis.Options{
 		Addr:     "redis:6379",
 		Password: "",
 		DB:       0,
 	})
 
-	searcher := func(userip string, term string) string {
-		cachedURL, err := redisClient.Get("todoy:" + term).Result()
+	fmt.Printf("Server started on %s\n", hostAndPort)
+
+	// HTTP handler to get request, parse subdomain, and send back the image URL
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		searchTerm := strings.ToLower(strings.Split(r.Host, ".")[0])
+
+		imageURL, err := redisClient.Get("todoy:" + searchTerm).Result()
 		if err != redis.Nil {
-			return cachedURL
+			imageURL = searcher(r.RemoteAddr, searchTerm)
+			redisClient.Set("todoy:"+searchTerm, imageURL, time.Second*120)
 		}
+		fmt.Printf("%s: %s\n", searchTerm, imageURL)
 
-		gis := GoogleImageSearch{}
-		err = gis.Search(userip, term)
-		if err != nil {
-			fmt.Println(err)
-			return "http://www.nedmartin.org/v3/amused/_img/something-went-terribly-wrong.jpg"
-		}
-		num := 0
-		// do random stuff
-		// rand.Seed(time.Now().UTC().UnixNano())
-		// num := rand.Intn(len(gis.ResponseData.Results))
+		w.Header().Set("Content-Type", "text/html")
+		w.Header().Set("Referer", imageURL)
+		fmt.Fprintf(w, "<html><img src=\"%s\"></html>\n", imageURL)
+	})
 
-		redisClient.Set("todoy:"+term, gis.ResponseData.Results[num].URL, time.Second*120)
-		return gis.ResponseData.Results[num].URL
+	http.ListenAndServe(hostAndPort, nil)
+}
+
+// method to perform a Google Image search
+func searcher(userip string, term string) string {
+	gis := GoogleImageSearch{}
+	err := gis.Search(userip, term)
+	if err != nil {
+		fmt.Println(err)
+		return "http://www.nedmartin.org/v3/amused/_img/something-went-terribly-wrong.jpg"
 	}
-
-	fmt.Println("Server started on :8080")
-	DoServer("/", ":8080", searcher)
+	num := 0
+	// do random stuff
+	// rand.Seed(time.Now().UTC().UnixNano())
+	// num := rand.Intn(len(gis.ResponseData.Results))
+	return gis.ResponseData.Results[num].URL
 }
